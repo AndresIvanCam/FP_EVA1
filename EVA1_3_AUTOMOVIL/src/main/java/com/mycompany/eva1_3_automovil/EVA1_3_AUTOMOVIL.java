665003f3192b83882d624e76c1739775c75e2ef9
AndresIvanCam <l24550050@chihuahua2.tecnm.mx>
2024-08-29 16:24:21 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/main/java/com/mycompany/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author DOWER
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+        String marca;       //Recomendable tener los nombres de las variables en ingles
+        String modelo;
+        int año;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "Ford";
+        modelo = "Mustang";
+        año = 2022;
+        precio = 999000.1;
+        System.out.println(marca);      //imprime el contenido de la variable marca
+        System.out.println(modelo);
+        System.out.println(año);
+        System.out.println(precio);
+    }
+}
---
+++ EVA1_4_TELEVISION/src/main/java/com/mycompany/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_television;
+
+/**
+ *
+ * @author DOWER
+ */
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+        String marca;
+        int pulgadas;
+        String resolucion;
+        double precio;
+        
+        marca = "LG";
+        pulgadas = 69;
+        resolucion = "HD";
+        precio = 1999.10;
+        
+        System.out.println(marca);
+        System.out.println(pulgadas);
+        System.out.println(resolucion);
+        System.out.println(precio);
+    }
+}
