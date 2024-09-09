46330ac22d702e09ed5a81f263b68f517ca41819
AndresIvanCam <l24550050@chihuahua2.tecnm.mx>
2024-09-09 16:13:21 -0600

    Add files via upload

---
+++ EVA1_8_OPERACIONES2/src/main/java/com/mycompany/eva1_8_operaciones2/EVA1_8_OPERACIONES2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_8_operaciones2;
+
+/**
+ *
+ * @author DOWER
+ */
+public class EVA1_8_OPERACIONES2 {
+
+    public static void main(String[] args) {
+        String Nombre = "Andres";
+        String Apellido = "Campos";
+        String NombreCom;
+        //Cadena de texto, la operacion se llama concatenacion --> unir cadena de texto
+        //El operador de concatenacion es el + cuan un simbolo tiene varias funciones se llama sobrecarga (de operadores)
+        NombreCom = Nombre +" "+ Apellido;
+        System.out.println(NombreCom);
+        
+        System.out.println("El Nombre es: "+Nombre);
+        System.out.println("El Apellido es: "+Apellido);
+        System.out.println("EL Nombre completo es: "+NombreCom);
+    }
+}
